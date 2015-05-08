ab648ac34fd1fb49e5daf2629b6f52e98a2aab7a
Chulwoo Lee <deadmeats@hotmail.com>
2015-05-07 23:32:36 -0400

    init file
    working ok.

--- init.c
+++ init.c
@@ -7,61 +7,60 @@
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
 
-const int bits = 2048;
-const int expo = 65537;
-//for random value at the end of .bank
 const int intmax = 2048;
 
 void getKeys(char *keys[]){
+	FILE *pri, *pub;	
+	char *line;
+	size_t leng = 0;
+	ssize_t lines;
+	int i, j;
+	
+	for(i = 0; i < 2; i++){	
+	char *prikey, *pubkey;
 
-   int pubkeylen, prikeylen;
-   char *pri_key, *pub_key;
-   RSA *rsa;
-   BIO *pubBio, *priBio;
-   int i, j;  //indexing purpose
+	prikey = malloc(4098*sizeof(char));
+	pubkey = malloc(4098*sizeof(char));	
 
-   //runs 2 times because 2 sets of pub/secret keys
-	for(i = 0; i < 2; i++){
-		//generate a pair of RSA key
-		rsa = RSA_generate_key(bits, expo, 0, 0);
+	system("openssl genrsa -out private.pem 2048");
+	system("openssl rsa -in private.pem -out public.pem -pubout");
 
-		/* getting string pem file form */ 
-		pubBio = BIO_new(BIO_s_mem());
-		priBio = BIO_new(BIO_s_mem());
+	pri = fopen("private.pem", "r");
+	pub = fopen("public.pem", "r");
 
-		PEM_write_bio_RSAPrivateKey(priBio, rsa, NULL, NULL, 0, NULL, NULL);
-		PEM_write_bio_RSAPublicKey(pubBio, rsa);
-	 
-		prikeylen = BIO_pending(priBio);
-		pubkeylen = BIO_pending(pubBio);
-
-		/* Null-terminate */
-		pri_key = calloc(prikeylen+1, 1);
-		pub_key = calloc(pubkeylen+1, 1);
-
-		BIO_read(priBio, pri_key, prikeylen);
-		BIO_read(pubBio, pub_key, pubkeylen);
-
-		if(i == 0){
-		   j = 3; 
-		}
-		else{
-		   j = 2;
-		}
-
-		keys[j] = malloc(prikeylen+1);
-		strncpy(keys[j], pri_key, prikeylen+1);
-		keys[i] = malloc(pubkeylen+1);
-		strncpy(keys[i], pub_key, pubkeylen+1);
-
-		BIO_free_all(priBio);
-		BIO_free_all(pubBio);
-		RSA_free(rsa);
-		free(pri_key);
-		free(pub_key);
+	if(pri == NULL || pub == NULL){
+		exit(0);
 	}
 
+	if(i == 0){
+	   j = 3; 
+	}
+	else{
+	   j = 2;
+	}
+
+	while ((lines = getline(&line, &leng, pri)) != -1){
+		strcat(prikey, line);
+	}
+
+	while ((lines = getline(&line, &leng, pub)) != -1){
+		strcat(pubkey, line);
+	}
+
+	keys[j] = malloc(strlen(prikey));
+	strncpy(keys[j], prikey, strlen(prikey));
+	keys[i] = malloc(strlen(pubkey));
+	strncpy(keys[i], pubkey, strlen(pubkey));
+
+	system("rm private.pem");
+	system("rm public.pem");
+
+	fclose(pri);
+	fclose(pub);
+	}
 }
+
+//-----BEGIN RSA PRIVATE KEY-----
 
 int main(int argc, char**argv){
 
@@ -74,61 +73,58 @@ int main(int argc, char**argv){
    
    
    //check if there is only one argument
-	if(argc != 2){
-		printf("Usage: init <filename>\n");
-		return 62;
-	}
+   if(argc != 2){
+	printf("Usage: init <filename>\n");
+	return 62;
+   }
    
-	atmpath = malloc(strlen(argv[1])+strlen(atmExt)+1); 
-	bankpath = malloc(strlen(argv[1])+strlen(bankExt)+1);
+   atmpath = malloc(strlen(argv[1])+strlen(atmExt)+1); 
+   bankpath = malloc(strlen(argv[1])+strlen(bankExt)+1);
 
-	if(atmpath != NULL || bankpath != NULL){
-		strcat(atmpath, argv[1]);
-		strcat(atmpath, atmExt);
-		strcat(bankpath, argv[1]);
-		strcat(bankpath, bankExt);
-	}
+   if(atmpath != NULL || bankpath != NULL){
+	strcat(atmpath, argv[1]);
+	strcat(atmpath, atmExt);
+	strcat(bankpath, argv[1]);
+	strcat(bankpath, bankExt);
+   }
    //check if file already exists
-	if( access(atmpath, F_OK) != -1 || access(bankpath, F_OK) != -1){
-		printf("Error: one of the files already exists\n");
-		return 63;
+   if( access(atmpath, F_OK) != -1 || access(bankpath, F_OK) != -1){
+	printf("Error: one of the files already exists\n");
+	return 63;
+   }
+
+   getKeys(keys);
+
+   if((atm = fopen(atmpath, "w"))==NULL || (bank = fopen(bankpath, "w"))==NULL){
+	printf("Error creating initialization files\n");
+	return 64;
+   }
+
+   for(i = 0; i < 4; i++){
+
+	if((i%2)==0){
+	   fputs(keys[i],bank);
 	}
-
-	getKeys(keys);
-
-	if((atm = fopen(atmpath, "w"))==NULL || (bank = fopen(bankpath, "w"))==NULL){
-		printf("Error creating initialization files\n");
-		return 64;
+	else{
+	   fputs(keys[i],atm);
 	}
+	free(keys[i]);
 
-   	for(i = 0; i < 4; i++){
-
-		if((i%2)==0){
-		   fputs(keys[i],bank);
-		}
-		else{
-		   fputs(keys[i],atm);
-		}
-		free(keys[i]);
-	}
-   
-   //generating random key at the end of .bank
+   }
    srand(time(0));
    random = rand();
    snprintf(randstr, sizeof(randstr), "%d", random);
    fputs(randstr, bank);
 
-	if(fclose(atm)||fclose(bank)){
-		printf("Error creating initialization files\n");
-		return 64;
-	}
+   if(fclose(atm)||fclose(bank)){
+	printf("Error creating initialization files\n");
+	return 64;
+   }
 	
-/*   
-if for any other reason the program fails, print 
+/*   • Otherwise, if for any other reason the program fails, print 
 “Error creating initialization files” and return value 64 
 (you do not need to delete or revert any files you may have created).
 */
-
    free(atmpath);
    free(bankpath);  
    printf("Successfully initialized bank state\n");
